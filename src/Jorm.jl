2414938a4c67de9cc6b6aa467b3e1b0baac13345
Jcharis <jcharistech@gmail.com>
2024-09-01 21:58:52 +0200

    chore: add crud fxns

--- src/Jorm.jl
+++ src/Jorm.jl
@@ -1,9 +1,14 @@
 module Jorm
 using SQLite
+using DataFrames
 
 # Write your package code here.
 export RawSQL,@raw_sql,tablename
 export connect,disconnect,SQLiteConnectionString
+export create_table,delete_table
+export read_one_sql,insert_sql,update_sql,delete_sql
+export read_one,insert!,update!,delete!
+
 
 """
     struct RawSQL  
@@ -72,7 +77,173 @@ function disconnect(db::SQLite.DB)::Nothing
 end
 
 
+"""
+    execute_query(db::SQLite.DB, query::RawSQL, params::Vector{Any}=Any[]) 
+    Returns the executed sql 
+"""
+function execute_query(db::SQLite.DB, query::RawSQL, params::Vector{Any}=Any[];toDF::Bool=false)
+    if toDF
+        return SQLite.DBInterface.execute(db, query.value, params) |> DataFrame
+    else
+        return SQLite.DBInterface.execute(db, query.value, params) 
+    end
+end
 
+"""
+    create_table(db::SQLite.DB, model, tableName)
+    Creates a table in the database based on the model.
+"""
+function create_table(db::SQLite.DB, model, tableName)
+    fields = fieldnames(model)
+    field_types = [typeof(getfield(model(), field)) for field in fields]
+    field_definitions = [ "$(field) $(typeof_to_sql(field_type))" for (field, field_type) in zip(fields, field_types) ]
+    query = "CREATE TABLE IF NOT EXISTS $tableName (id INTEGER PRIMARY KEY AUTOINCREMENT, $(join(field_definitions, ", ")))"
+    SQLite.execute!(db, query)
+end
+
+function typeof_to_sql(field_type)
+        if field_type == Int
+            return "INTEGER"
+        elseif field_type == String
+            return "TEXT"
+        else
+            error("Unsupported field type: $field_type")
+        end
+end
+
+function delete_table(db::SQLite.DB, model) 
+    query = "DROP TABLE IF EXISTS $(tablename(model))"
+    DBInterface.execute(db, query)
+end
+
+"""
+    list_tables(connection_string::SQLiteConnectionString) 
+    Returns the list of tables in DB 
+"""
+function list_tables(connection_string::SQLiteConnectionString)
+    # Connect to the database
+    db = SQLite.DB(connection_string.database_name)
+    # Get the list of tables
+    tables = SQLite.tables(db)
+    SQLite.close(db)
+    return tables
+    
+end
+
+"""
+    ls_tables(connection_string::SQLiteConnectionString) 
+    Returns the list of tables in DB 
+"""
+function ls_tables(connection_string::Jorm.SQLiteConnectionString)
+    db = SQLite.DB(connection_string.database_name)
+    query = "SELECT name FROM sqlite_master WHERE type='table'"
+    results = DBInterface.execute(db, query)
+    tables = [row.name for row in results]
+    close(db)
+    return tables
+end
+
+
+"""
+    delete_all(db::SQLite.DB, model) 
+    Deletes all data in the table for the given model
+"""
+function delete_all!(db::SQLite.DB, model)
+    query = "DELETE FROM $(tablename(model))"
+    DBInterface.execute(db, query)
+end
+
+
+
+
+# Function to generate the SQL query for reading one record
+function read_one_sql(model, id)
+    query = "SELECT * FROM $(tablename(model)) WHERE id = ?"
+    return query
+end
+
+# Function to generate the SQL query for creating a new record
+function insert_sql(model, data)
+    columns = join(fieldnames(model), ", ")
+    placeholders = join(repeat("?", length(fieldnames(model))), ", ")
+    query = "INSERT INTO $(tablename(model)) ($columns) VALUES ($placeholders)"
+    return query
+end
+
+# Function to generate the SQL query for updating a record
+function update_sql(model, id, data)
+    columns = join([string(field, " = ?") for field in fieldnames(model)], ", ")
+    query = "UPDATE $(tablename(model)) SET $columns WHERE id = ?"
+    return query
+end
+
+# Function to generate the SQL query for deleting a record
+function delete_sql(model, id)
+    query = "DELETE FROM $(tablename(model)) WHERE id = ?"
+    return query
+end
+
+# Wrapper function to display the SQL syntax
+function show_sql(func, args...)
+    if func == insert!
+        return insert_sql(args, args)
+    elseif func == update!
+        return update_sql(args, args, args)
+    elseif func == delete!
+        return delete_sql(args, args)
+    elseif func == read_one
+        return read_one_sql(args, args)
+    else
+        error("Unsupported function")
+    end
+end
+
+# CRUD functions using the above SQL generation and execution
+
+"""
+    read_one(db::SQLite.DB, model, id) 
+    Returns a given model object when given the ID  
+"""
+function read_one(db::SQLite.DB, model, id)
+    query = read_one_sql(model, id)
+    params = Any[id]
+    result = Jorm.execute_query(db, query, params)
+    return result
+end
+
+
+"""
+    insert!(db::SQLite.DB, model, data) 
+    Insert or Add data to the DB for a given model
+"""
+function insert!(db::SQLite.DB, model, data)
+    query = insert_sql(model, data)
+    params = Any[getfield(data, field) for field in fieldnames(model)]
+    Jorm.execute_query(db, query, params)
+end
+
+
+"""
+    update!(db::SQLite.DB, model, id, data)
+    Update data to the DB for a given model
+"""
+function update!(db::SQLite.DB, model, id, data)
+    query = update_sql(model, id, data)
+    params = Any[getfield(data, field) for field in fieldnames(model)]
+    push!(params, id)
+    Jorm.execute_query(db, query, params)
+end
+
+
+"""
+    delete!(db::SQLite.DB, model, id)
+    Delete data for a given model
+"""
+function delete!(db::SQLite.DB, model, id)
+    query = delete_sql(model, id)
+    params = Any[id]
+    Jorm.execute_query(db, query, params)
+end
 # crud as sql 
 # crud 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -27,3 +27,137 @@ end
     Jorm.disconnect(db)
 
 end
+
+
+struct BlogPost
+    id::Int 
+    title::String 
+    content::String 
+end
+
+# Test setup: Create a temporary database for testing
+function setup_test_db()
+    db = SQLite.DB("test.db")
+    SQLite.execute(db, "CREATE TABLE IF NOT EXISTS test_table (id INTEGER PRIMARY KEY, name TEXT)")
+    SQLite.execute(db, "INSERT INTO test_table (id, name) VALUES (1, 'John')")
+    return db
+end
+
+# Test teardown: Close and delete the temporary database
+function teardown_test_db(db)
+    SQLite.close(db)
+    rm("test.db")
+end
+
+@testset "Test Utils" begin
+    @test Jorm.tablename(BlogPost) == "blog_post"
+    @test @raw_sql("SELECT * FROM my_table") == Jorm.RawSQL("SELECT * FROM my_table")
+end
+
+
+# Test that the connection is established successfully
+@testset "Connection Tests" begin
+    # Test with a valid connection string
+    connection_string = Jorm.SQLiteConnectionString(database_name="test.db")
+    db = Jorm.connect(connection_string)
+    @test typeof(db) == SQLite.DB
+    Jorm.disconnect(db)
+
+    # Test with an invalid connection string (this should throw an error)
+    # invalid_connection_string = "invalid_path"
+    # @test_throws SQLite.Error Jorm.connect(invalid_connection_string)
+end
+
+# Test that the query executes successfully and returns the correct result
+@testset "Execute Query Tests" begin
+    db = setup_test_db()
+    query = Jorm.RawSQL("SELECT * FROM test_table WHERE id = ?")
+    params = Any["John"]
+    result = Jorm.execute_query(db, query, params)
+    println(result)
+    for row in result
+        @test row.id == 1
+        @test row.name == "John"
+    end
+    # insert new 
+    query2 = Jorm.RawSQL("INSERT INTO test_table (id, name) VALUES (?, ?)")
+    params2 = Any[2, "Peter"]
+    result2 = Jorm.execute_query(db, query2, params2)
+    println(result2)
+    for row in result2
+        @test row.id == 2
+        @test row.name == "Peter"
+    end
+    query3 = Jorm.RawSQL("INSERT INTO test_table (id, name) VALUES (?, ?)")
+    params3 = Any[3, "Petro"]
+   
+    result2df = Jorm.execute_query(db, query3, params3,toDF=true)
+    # @test result2df.id == 3
+    # @test result2df.name == "Petro"
+    teardown_test_db(db)
+end
+
+@testset "List Tables" begin
+    connection_string = Jorm.SQLiteConnectionString(database_name="test.db")
+    result = Jorm.list_tables(connection_string)
+    for r in result
+        @test r === nothing
+    end
+end
+
+
+@testset "Test Show SQL Constructs" begin
+    # Example usage:
+    struct MyModel
+        id::Int
+        name::String
+        age::Int
+    end
+
+
+    data = MyModel(1, "John Doe", 30)
+
+    @test read_one_sql(MyModel, 1) == "SELECT * FROM my_model WHERE id = ?"
+    @test insert_sql(MyModel, data) == "INSERT INTO my_model (id, name, age) VALUES (?, ?, ?)"
+    @test update_sql(MyModel, 1, data) == "UPDATE my_model SET id = ?, name = ?, age = ? WHERE id = ?"
+    @test delete_sql(MyModel, 1) == "DELETE FROM my_model WHERE id = ?"
+
+end
+
+# @testset "CRUD Test" begin
+#     struct Blog
+#         id::Int
+#         name::String
+#     end
+#     connection_string = Jorm.SQLiteConnectionString(database_name="test.db")
+#     tb = Jorm.tablename(Blog)
+#     db = Jorm.connect(connection_string)
+#     Jorm.create_table(db,Blog,tb)
+    
+#     # Create a new record
+#     data = Blog(1, "My Blog Post")
+#     Jorm.insert!(db, Blog, data)
+
+#     # Read all records
+#     results = Jorm.read_all(db, Blog)
+#     for row in results
+#         @test row.id == 1
+#     end
+
+#     # Update an existing record
+#     updated_data = Blog(1, "Updated Blog Post")
+#     Jorm.update!(db, Blog, 1, updated_data)
+
+#     # Read one record by ID
+#     result = Jorm.read_one(db, Blog, 1)
+#     println(result)
+
+#     for row in results
+#         @test row.id == 1
+#     end
+
+#     # Close the database connection
+#     Jorm.disconnect(db)
+
+
+# end
